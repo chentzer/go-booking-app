dc8bdfd5944d4e7d96043bec3668fa0d314f97af
Liew Chen Tzer <liewct115@gmail.com>
2026-02-03 16:41:18 +0800

    variable & constant

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	//print
-	fmt.Print("Hello World")
-	//print line
-	fmt.Println("Hello World")
+	//variable - value can change
+	var conferenceName = "Go Conference"
+	//constants - value cannot change
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("Get your tickets here to attend!")
+	fmt.Println("We have", conferenceTickets, "tickets! Currenlty", remainingTickets, "tickets left available now!!")
 }
