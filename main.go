0492000b33068abbc650514c1d867590ba21e026
Liew Chen Tzer <liewct115@gmail.com>
2026-02-24 02:03:12 +0800

    datatype

--- main.go
+++ main.go
@@ -9,7 +9,17 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 30
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking applicatio\n", conferenceName)
 	fmt.Println("Get your tickets here to attend!")
 	fmt.Printf("We have %v tickets! Currenlty %v tickets left available now!!", conferenceTickets, remainingTickets)
+
+	var userName string
+	var userTickets int
+	// ask user for thier name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Println("User %v booked %v tickets.\n", userName, userTickets)
 }
